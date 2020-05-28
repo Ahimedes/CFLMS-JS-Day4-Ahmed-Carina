5294966869b10e479b3643c5d7bea4a3f7648213
Ahmed Abdelaal <ahmed.abdellaoue@gmail.com>
2020-05-28 11:27:31 +0200

    First exercise

---
+++ Basic/index1.js
@@ -0,0 +1,6 @@
+function display  () {
+	var node1 = document.getElementById("input1").value;
+	var node2 = document.getElementById("input2").value;
+	var node3 = document.getElementById("input3").value;
+	alert(node1)
+}
