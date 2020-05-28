39d1f9f7ab242192050f0f486e05fe9428190467
Ahmed Abdelaal <ahmed.abdellaoue@gmail.com>
2020-05-28 13:49:05 +0200

    Second basic exercise

--- Basic/index1.js
+++ Basic/index1.js
@@ -2,14 +2,24 @@ function display  () {
 	var node1 = document.getElementById("input1").value;
 	var node2 = document.getElementById("input2").value;
 	var node3 = document.getElementById("input3").value;
-	var result1 = document.getElementById("demo1").innerHTML = node1;
-	var result2 = document.getElementById("demo2").innerHTML = node2;
-	var result3 = document.getElementById("demo3").innerHTML = node3;
+	var node4 = document.getElementById("input4").value;
+	document.getElementById("demo1").innerHTML = "First name: " + node1;
+	document.getElementById("demo2").innerHTML = "Last name: " + node2;
+	document.getElementById("demo3").innerHTML = "Age: " + node3;
+	document.getElementById("demo4").innerHTML = "Profession: " + node4;
 	if (node1.length < 5) {
 	document.getElementById("demo1").style.color = "red";
 	}
 	else {
 		document.getElementById("demo1").style.color = "green";
+	}
+	if (node4 == "IT") {
+		document.getElementById("demo4").style.backgroundColor = "purple";
+		document.getElementById("demo4").style.color = "white"
+	}
+	else {
+		document.getElementById("demo4").style.backgroundColor = "blue";
+		document.getElementById("demo4").style.color = "white"
 	}
 }
 	
